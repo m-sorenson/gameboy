2511ccf54f45b822909fd637eacb9240fd6e2eb5
Michael Sorenson <m.sorenson407@gmail.com>
2016-03-15 22:41:54 -0600

    Takes rom path as arg. Reads moves from stdin.

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 'use strict';
 
-let fs       = require('fs');
-let gameboy  = require('gameboy');
-let Canvas   = require('canvas');
-let canvas   = new Canvas(160, 144);
+let fs      = require('fs');
+let gameboy = require('gameboy');
+let Canvas  = require('canvas');
+let canvas  = new Canvas(160, 144);
 
-fs.readFile('/home/michael/Downloads/Pokemon Yellow.gb', function(err, rom) {
-  let game = gameboy(canvas, rom, { drawEvents: true });
+if (process.argv.length < 3) {
+  throw new Error('Must supply path to ROM.')
+} else {
+  fs.readFile(process.argv[2], function(err, rom) {
+    let game = gameboy(canvas, rom, { drawEvents: true });
 
-  game.stopEmulator = 1;
-  setInterval(game.run.bind(game), 8);
-  game.on('draw', function() {
-    canvas.toBuffer(function(err, buff) {
-      if (err) throw err;
-      let encoded = buff.toString('base64');
-      console.log(encoded);
+    game.stopEmulator = 1;
+    setInterval(game.run.bind(game), 8);
+    game.on('draw', function() {
+      canvas.toBuffer(function(err, buff) {
+        if (err) throw err;
+        let encoded = buff.toString('base64');
+        console.log(encoded);
+      });
     });
-  });
 
-  process.stdin.resume();
-  process.stdin.setEncoding('utf8');
-  process.stdin.on('data', function(move) {
-    move = move.trim()
-    game.JoyPadEvent(move, true);
-    setTimeout(function() {
-      game.JoyPadEvent(move, false)
-    }, 50);
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
+    process.stdin.on('data', function(move) {
+      move = move.trim()
+      game.JoyPadEvent(move, true);
+      setTimeout(function() {
+        game.JoyPadEvent(move, false)
+      }, 50);
+    });
+    game.start();
   });
-  game.start();
-});
+}
